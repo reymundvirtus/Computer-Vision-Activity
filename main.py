e4c58001f992768a66ed36d22657cbdbbacc8569
reymundvirtus <reymundvirtus@outlook.com>
2022-04-12 12:03:17 +0800

    PE18 Rotate Image using Geometric Transformation

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import cv2 as cv
 
-image = cv.imread('motor.jpg') # Read original image
+image = cv.imread('motor.jpg', 0) # Read original image
 
-grayscale = cv.cvtColor(image, cv.COLOR_BGR2GRAY)  # Convert to grayscale
+rows, cols = image.shape
 
-blackAndWhiteImage = cv.threshold(grayscale, 90, 255, cv.THRESH_BINARY)[1] # Convert grayscale image to black and white
+# cols-1 and rows-1 are the coordinate limits.
+fortyFive = cv.getRotationMatrix2D(((cols - 1) / 2.0, (rows - 1) / 2.0), 45, 0.9) # adjusted to 0.9 to fit the image in window
+dst45 = cv.warpAffine(image, fortyFive, (cols, rows))
 
-blackAndWhiteImageCopy = cv.threshold(image, 127, 255, cv.THRESH_BINARY)[1] # Convert original image to black and white
-negativeImage = cv.bitwise_not(blackAndWhiteImageCopy) # Invert black and white image to negative image
+oneEighty = cv.getRotationMatrix2D(((cols - 1) / 2.0, (rows - 1) / 2.0), 180, 1)
+dst180 = cv.warpAffine(image, oneEighty, (cols, rows))
 
-cv.imshow('Original Image', image) # Show original image
-cv.imshow('Gray Image', grayscale) # Show gray image
-cv.imshow('Inverted Image', blackAndWhiteImage) # Show inverted image
-cv.imshow('Negative Image', negativeImage) # Show negative image
+ninety = cv.getRotationMatrix2D(((cols - 1) / 2.0, (rows - 1) / 2.0), 90, 0.7) # adjusted to 0.7 to fit the image in window
+dst90 = cv.warpAffine(image, ninety, (cols, rows))
+
+twoSeventy = cv.getRotationMatrix2D(((cols - 1) / 2.0, (rows - 1) / 2.0), 270, 0.7) # adjusted to 0.7 to fit the image in window
+dst270 = cv.warpAffine(image, twoSeventy, (cols, rows))
+
+cv.imshow('45', dst45) # Show 45 degree image
+cv.imshow('180', dst180) # Show 180 degree image
+cv.imshow('90', dst90) # Show 90 degree image
+cv.imshow('270', dst270) # Show 270 degree image
 k = cv.waitKey(0) # wait for key to be pressed
