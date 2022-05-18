93fa6a6d888b54b9b43b47cc517ed2ee113c6025
reymundvirtus <reymundvirtus@outlook.com>
2022-05-18 17:23:02 +0800

    PE24 Grayscale Histogram and Flattered Histogram

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 
 # original image
-image = cv.imread('itachi.jpg') # read image
+image = cv.imread('motor.jpg') # read image
 cv.imshow('Original', image) # show original image
 
-mask = np.zeros(image.shape[:2], dtype = np.uint8)
-cv.circle(mask, (310, 280), 65, 255, -1) # left eye
-cv.circle(mask, (570, 180), 70, 255, -1) # right eye
-cv.rectangle(mask, (180, 10), (570, 100), 255, -1) # rouge ninja bandage
-cv.imshow('Mask', mask)
+def histogram(image):
+    grayscale = cv.cvtColor(image, cv.COLOR_BGR2GRAY) # convert to grayscale
+    cv.imshow('Grayscale', grayscale) # show grayscale image
+    hist = cv.calcHist([grayscale], [0], None, [256], [0, 256]) # calculate histogram
+    plt.figure()
+    plt.title('Grayscale Histogram')
+    plt.xlabel('Bins')
+    plt.ylabel('no. of Pixels')
+    plt.plot(hist)
+    plt.xlim([0, 256])
+    plt.show()
 
-masked = cv.bitwise_and(image, image, mask = mask)
-cv.imshow('Mask Applied to Image', masked)
+def flatten(image):
+    chans = cv.split(image) # split image into channels
+    colors = ("b", "g", "r") # color names
+    plt.figure()
+    plt.title("Flattened Color Histogram")
+    plt.xlabel("Bins")
+    plt.ylabel("no. of Pixels")
+    for (chan, color) in zip(chans, colors):
+        hist = cv.calcHist([chan], [0], None, [256], [0, 256])
+        plt.plot(hist, color = color)
+        plt.xlim([0, 256])
+    plt.show()
+
+histogram(image)
+flatten(image)
 cv.waitKey(0)
 cv.destroyAllWindows()
